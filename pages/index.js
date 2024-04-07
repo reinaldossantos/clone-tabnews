ed82fcc3a8b664c683a8890de4234de0803cb717
reinaldossantos <104236026+reinaldossantos@users.noreply.github.com>
2024-04-07 04:11:04 +0000

    adiciona os arquivos `lint:fix e lint:write`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home(){
-    return <h1>Adriana Eu amo Você, se Você me ama, dá uma risadinha...</h1>
-    
+function Home() {
+  return <h1>Adriana Eu amo Você, se Você me ama, dá uma risadinha...</h1>;
 }
 export default Home;
+
+function Home1() {
+  return <h2></h2>;
+}
